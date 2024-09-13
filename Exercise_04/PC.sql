2a5815494e4e44cfc760c57cd09680dfb3023863
Danail Todorov <danailtodorov440@gmail.com>
2024-09-13 16:36:26 +0300

    Update PC.sql

--- Exercise_04/PC.sql
+++ Exercise_04/PC.sql
@@ -1,17 +1,21 @@
---tuka nqma li da stane taka, smsl ako model v product nqma s kvo da se zapulni to shte e NULL
---ne stava, shtoto realno nqmam dopulnitelna kolona, na koqto da izpishe null i da moga da proverq chrez neq
+--1. Напишете заявка, която извежда производител, модел и тип на продукт
+--за тези производители, за които съответният продукт не се продава
+--(няма го в таблиците PC, Laptop или Printer)
+--v1
 SELECT DISTINCT product.maker, product.model, product.type
 FROM product
 left JOIN pc ON product.model = pc.model
 LEFT JOIN printer ON product.model = printer.model
 LEFT JOIN laptop ON product.model = laptop.model
-
+--v2
 SELECT * 
 FROM product
 WHERE product.model NOT IN (SELECT laptop.model FROM laptop) AND
 product.model NOT IN (SELECT pc.model FROM pc) AND
 product.model NOT IN (SELECT printer.model FROM printer)
---
+  
+--2. Намерете всички производители, които правят както лаптопи, така и
+--принтери.
 SELECT DISTINCT p1.maker
 FROM product AS p1
 JOIN product AS p2 ON p1.maker = p2.maker
@@ -24,13 +28,16 @@ INTERSECT
 (SELECT product.maker
 FROM product
 WHERE product.type = 'Laptop')
---
 
+--3. Намерете размерите на тези твърди дискове, които се появяват в два
+--или повече модела лаптопи.
 SELECT DISTINCT l1.hd
 FROM laptop AS l1
 JOIN laptop AS l2 ON l1.hd = l2.hd
 WHERE l1.model < l2.model
 
+--4. Намерете всички модели персонални компютри, които нямат регистриран
+--производител.
 SELECT *
 FROM pc
 LEFT JOIN product ON pc.model = product.model
