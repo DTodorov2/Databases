236b28f2a1582c5f52badb241d31ae702a2d8c48
Danail Todorov <danailtodorov440@gmail.com>
2024-09-13 17:02:52 +0300

    Update SQLQuery6.sql

--- Exercise_11/SQLQuery6.sql
+++ Exercise_11/SQLQuery6.sql
@@ -1,3 +1,5 @@
+--1. Добавете нова колона num_pass към таблицата Flights, която ще съдържа броя на
+пътниците, потвърдили резервация за съответния полет.
 ALTER TABLE FLIGHTS
 ADD num_pass int;
 
@@ -17,6 +19,8 @@ UPDATE FLIGHTS
 SET num_pass = 0
 WHERE num_pass IS NULL
 
+--2. Добавете нова колона num_book към таблицата Agencies, която ще съдържа броя на
+резервациите към съответната агенция.
 ALTER TABLE AGENCIES
 ADD num_book int;
 
@@ -32,6 +36,9 @@ UPDATE AGENCIES
 SET num_book = 0
 WHERE num_book IS NULL
 
+--3. Създайте тригер за таблицата Bookings, който да се задейства при вмъкване на
+--резервация в таблицата и да увеличава с единица броя на пътниците, потвърдили
+--резервация за таблицата Flights, както и броя на резервациите към съответната агенция.
 CREATE TRIGGER task03
 ON BOOKINGS AFTER INSERT
 AS
@@ -45,6 +52,9 @@ WHERE AGENCIES.NAME IN (SELECT NAME FROM inserted)
 INSERT INTO BOOKINGS
 VALUES ('YN298P', 'Travel One', 'FB', 'FB1363', 1, '2021-04-27', '2021-12-25', 300, 0)
 
+--4. Създайте тригер за таблицата Bookings, който да се задейства при изтриване на
+--резервация в таблицата и да намалява с единица броя на пътниците, потвърдили
+--резервация за таблицата Flights, както и броя на резервациите към съответната агенция.
 CREATE TRIGGER task04
 ON BOOKINGS AFTER DELETE
 AS BEGIN
@@ -56,6 +66,9 @@ SET num_book = num_book - 1
 WHERE AGENCIES.NAME IN (SELECT NAME FROM deleted)
 END
 
+--5. Създайте тригер за таблицата Bookings, който да се задейства при обновяване на
+--резервация в таблицата и да увеличава или намалява с единица броя на пътниците,
+--потвърдили резервация за таблицата Flights при промяна на статуса на резервацията.
 CREATE TRIGGER task05
 ON BOOKINGS AFTER UPDATE
 AS BEGIN
